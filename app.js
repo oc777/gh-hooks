d12ab7fb61766c6378bbe0b1fc891d9472824567
oc777 <oc222ba@student.lnu.se>
2019-08-24 18:35:52 +0200

    app basic config

---
+++ app.js
@@ -0,0 +1,92 @@
+'use strict'
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const path = require('path')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// Parse application/x-www-form-urlencoded.
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// setup session
+const sessionOptions = {
+  name: 'snippetsapp',
+  secret: 'humptydumpty',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    // secure: true, // for HTTPS connection
+    maxAge: 1000 * 60 * 60, // 60 minutes
+    sameSite: 'lax'
+  }
+}
+
+app.use(session(sessionOptions))
+
+// Configure rendering engine, with change extension to .hbs
+app.engine('hbs', exphbs({
+  extname: '.hbs',
+  defaultLayout: path.join(__dirname, 'views', 'layouts', 'main'),
+  partialsLayout: path.join(__dirname, 'views', 'partials')
+}))
+
+// Setup view engine.
+app.set('view engine', 'hbs')
+
+// Serve static files.
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Setup flash messages.
+app.use((req, res, next) => {
+  res.locals.flash = req.session.flash
+  delete req.session.flash
+  next()
+})
+
+// routes
+app.use('/', require('./routes/home.js'))
+
+// catch 404
+app.use((req, res, next) => {
+  res.status(404)
+  res.render(path.join(__dirname, 'views', 'errors/404.hbs'))
+})
+
+// custom error handling
+app.use((err, req, res, next) => {
+  // dev print err
+  console.log(err)
+
+  // 401 - lacks valid authentication
+  if (err.statusCode === 401) {
+    console.log('401 err')
+    return res.status(401).render(path.join(__dirname, 'views', 'errors/401.hbs'))
+  }
+
+  // 403 - not authorized
+  if (err.statusCode === 403) {
+    console.log('403 err')
+    return res.status(403).render(path.join(__dirname, 'views', 'errors/403.hbs'))
+  }
+
+  // 404 - not found
+  if (err.statusCode === 404) {
+    console.log('404 err')
+    console.log(err)
+    return res.status(404).render(path.join(__dirname, 'views', 'errors/404.hbs'))
+  }
+
+  // 500 - server error
+  console.log('server err')
+  res.status(err.status || 500).render(path.join(__dirname, 'views', 'errors/500.hbs'))
+})
+
+// Start listening.
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
