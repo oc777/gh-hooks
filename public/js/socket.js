756b5f9fcea0b91b11341597f30dae18dc8912d6
oc777 <oc222ba@student.lnu.se>
2019-08-26 01:21:37 +0200

    payload rendering

--- public/js/socket.js
+++ public/js/socket.js
@@ -22,7 +22,6 @@ let renderUI = (data) => {
 
   let eventTitle = document.createElement('h2')
   let eventAction = document.createElement('p')
-  let eventBody = document.createElement('p')
 
   let titleTxt = (data.action) ? ((data.action.comment) ? 'Issue Comment' : 'Issue') : 'Push Notification'
   let title = document.createTextNode(titleTxt)
@@ -30,16 +29,11 @@ let renderUI = (data) => {
   let actionTxt = data.action || ('Pusher: ' + data.pusher.name)
   let action = document.createTextNode(actionTxt)
 
-  let bodyTxt = (data.issue) ? ((data.label.name) ? data.label.name : data.issue.body ) : ''
-  let body = document.createTextNode(bodyTxt)
-
   eventTitle.appendChild(title)
   eventAction.appendChild(action)
-  eventBody.appendChild(body)
 
   div.appendChild(eventTitle)
   div.appendChild(eventAction)
-  div.appendChild(eventBody)
 
   let htmlbody = document.getElementsByTagName("BODY")[0]
   htmlbody.appendChild(div)
