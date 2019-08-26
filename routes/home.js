639987d29deb316ed16c2a0e912d2ae91da0be61
oc777 <oc222ba@student.lnu.se>
2019-08-26 04:07:29 +0200

    get all issues

--- routes/home.js
+++ routes/home.js
@@ -1,26 +1,20 @@
 'use strict'
 
 const router = require('express').Router()
-const https = require('https')
+const fetch = require('node-fetch')
 
 router.get('/', (req, res, next) => {
-  https.get(`https://api.github.com/repos/1dv023/oc222ba-examination-3/issues?access_token=${process.env.GIT_AUTH}`, (resp) => {
-    let issues = []
+  let issues = getIssues()
+  console.log(issues)
 
-    // A chunk of data has been recieved.
-    resp.on('data', (chunk) => {
-      issues += chunk
-    })
-
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-      console.log(JSON.parse(issues))
-    })
-  }).on('error', (err) => {
-    console.log('Error: ' + err.message)
-  })
 
   res.render('home/index')
 })
+
+let getIssues = () => {
+    let res = await fetch(`https://api.github.com/repos/1dv023/oc222ba-examination-3/issues?access_token=${process.env.GIT_AUTH}`)
+    let json = await res.json()
+    return json
+}
 
 module.exports = router
